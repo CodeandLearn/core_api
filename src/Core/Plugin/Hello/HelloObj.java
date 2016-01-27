8c3f78a0afbfaaaacf3404d7a8809828fa452602
tfSheol <teddy.fontaine@epitech.eu>
2016-01-27 22:14:09 +0100

    Fix empty & not null

--- src/Core/Plugin/Hello/HelloObj.java
+++ src/Core/Plugin/Hello/HelloObj.java
@@ -3,37 +3,41 @@ package Core.Plugin.Hello;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
 
 public class HelloObj {
-	@NotNull
+	@NotEmpty
+	@Size(max = 16)
 	private String name;
-	@NotNull
+	@NotEmpty
 	private String title;
 	@NotNull
 	@Min(1)
-	@Max(10)
+	@Max(value = 10, message = "test de message d'erreur")
 	private Integer id;
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public String getTitle() {
 		return title;
 	}
-	
+
 	public Integer getId() {
 		return id;
 	}
