6478b912651e9252dd57cd01a354c6206a08a10a
tfSheol <teddy.fontaine@epitech.eu>
2016-02-12 09:44:53 +0000

    Fix port

--- src/Core/CustomizationBean.java
+++ src/Core/CustomizationBean.java
@@ -12,7 +12,7 @@ public class CustomizationBean implements EmbeddedServletContainerCustomizer {
 	public void customize(ConfigurableEmbeddedServletContainer container) {
 		MimeMappings mappings = new MimeMappings(MimeMappings.DEFAULT);
 		mappings.add("Content-Type", "application/json; charset=UTF-8");
-		container.setPort(8081);
+		container.setPort(8080);
 		container.setSessionTimeout(10, TimeUnit.MINUTES);
 		// container.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND,
 		// "/notfound.html"));
