0e450e155ee2cbc7888f695e9d63cf89129dd024
Raphael <morand.raphael.13@gmail.com>
2016-11-03 16:39:06 +0100

    feedback objects

---
+++ src/Plugin/FeedBack/Objects/FeedBackObj.java
@@ -0,0 +1,17 @@
+package Plugin.FeedBack.Objects;
+
+/**
+ * Created by moran on 03/11/2016.
+ */
+public class FeedBackObj {
+
+    public int id;
+    public int account_id;
+    // Data tables feedback_types, feedback_status, feedback_platforms;
+    public int type_id;
+    public int status_id;
+    public int platform_id;
+    // TimeStamp
+    public long date;
+
+}
---
+++ src/Plugin/FeedBack/Objects/FeedBackStatus.java
@@ -0,0 +1,9 @@
+package Plugin.FeedBack.Objects;
+
+/**
+ * Created by moran on 03/11/2016.
+ */
+public class FeedBackStatus {
+    public int id;
+    public String status_label;
+}
