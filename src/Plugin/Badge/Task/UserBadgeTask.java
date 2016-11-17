b4b5ab99dddddce769f22e719cc71ee83971d2a5
Teddy Fontaine <teddy.fontaine@epitech.eu>
2016-11-17 17:48:52 +0100

    Add more badges events

--- src/Plugin/Badge/Task/UserBadgeTask.java
+++ src/Plugin/Badge/Task/UserBadgeTask.java
@@ -21,16 +21,19 @@ public class UserBadgeTask extends Job {
         for (Map user : users) {
             int nb = new GetCourse().getAccountCourses(user.getInt("id")).getData().size();
             if (nb >= 5) {
-                new UserBadgeModel().postAccountBadge(user.getInt("id"), 2);
+                new UserBadgeModel().postAccountBadge(user.getInt("id"), 6); //publish 5 courses
             }
             if (nb >= 10) {
-                new UserBadgeModel().postAccountBadge(user.getInt("id"), 3);
+                new UserBadgeModel().postAccountBadge(user.getInt("id"), 7); //publish 10 courses
             }
             if (nb >= 30) {
-                new UserBadgeModel().postAccountBadge(user.getInt("id"), 4);
+                new UserBadgeModel().postAccountBadge(user.getInt("id"), 8); //publish 30 courses
             }
             if (nb >= 50) {
-                new UserBadgeModel().postAccountBadge(user.getInt("id"), 5);
+                new UserBadgeModel().postAccountBadge(user.getInt("id"), 9); //publish 50 courses
+            }
+            if (user.getLong("create_timestamp") > 1479963600000L && user.getLong("create_timestamp") < 1480197600000L) { //Epitech Experience dates in timestamp
+                new UserBadgeModel().postAccountBadge(user.getInt("id"), 10);  //Epitech Experience 2016
             }
         }
     }
